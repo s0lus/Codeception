37af9b668b29009f24ffd05ae3f0b35e047d9e8d
s0lus <mashenkoalex@gmail.com>
2021-04-07 19:02:26 +0300

    Add tests

--- tests/coverage/LocalCept.php
+++ tests/coverage/LocalCept.php
@@ -10,3 +10,8 @@ $I->amGoingTo('run local codecoverage in cest');
 $I->executeCommand('run math MathCest --coverage', null, '-d pcov.directory=' . getcwd());
 $I->seeInShellOutput('Classes: 100.00%');
 $I->seeInShellOutput('Methods: 100.00%');
+
+$I->amGoingTo('run local codecoverage with path and branch coverage');
+$I->executeCommand("run -o 'coverage: path_coverage: true' math MathTest --coverage", null);
+$I->seeInShellOutput('Paths: 100.00%');
+$I->seeInShellOutput('Branches: 100.00%');
